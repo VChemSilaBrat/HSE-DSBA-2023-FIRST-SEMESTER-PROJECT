ee095239ec865e872e93c48ba42ace3815801f32
Bebrochka <glonomess@protonmail.com>
2023-12-03 19:33:35 +0300

    html appear

--- main_flask.py
+++ main_flask.py
@@ -3,9 +3,13 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/about')
 def home():
-    return 'Это главная страница.'
+    return render_template("About.html")
+
+@app.route('/plots')
+def plots():
+    return render_template("Plots.html")
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
