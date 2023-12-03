71cc8c1d33c337148d20e27cb9d7faeb18ce18ce
Bebrochka <glonomess@protonmail.com>
2023-12-03 21:05:23 +0300

    adding all_month and fixing style

--- main_flask.py
+++ main_flask.py
@@ -6,6 +6,8 @@ import pandas as pd
 app = Flask(__name__)
 
 df = pd.read_csv("data_parsed.csv")
+df["all_month"] = df["Year"] * 12  + df['Month']
+# print(df)
 
 @app.route('/about')
 def home():
@@ -20,13 +22,13 @@ def plots():
         start_year = int(start.split(".")[1])
         end_month = int(end.split(".")[0])
         end_year = int(end.split(".")[1])
-        gdf = df[(df["Month"] >= start_month) & (df["Year"] >= start_year) & (df["Month"] <= end_month) & (df["Year"] <= end_year)]
+        gdf = df[(df["all_month"] >= start_month + start_year*12) & (df["all_month"] <= end_year * 12 + end_month)]
         # print(gdf)
         # print(start_month, start_year, end_month, end_year, sep = '/n')
         fig = px.line(
             gdf,
             x='datum',
-            y=gdf.columns[1:-2],
+            y=gdf.columns[1:-3],
             title='Your line graph'
         )
         fig_html = fig.to_html(full_html=False)
